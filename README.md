b222f48e9357fe729d8893f93c47de2c738089c1
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2022-10-28 15:35:50 +0530

    Add Single Topic Example

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# cdc-monitoring
-Tools to monitor cdc
+# CDC Examples
+
+Example Pipelines for Yugabyte CDC.
+
+## Table of Contents
+
+|Example|Description|
+|-------|-----------|
+|[Quick Start](cdc-quickstart-kafka-connect/README.md)|Setup a complete CDC
+pipeline using Docker containers in a single VM to Postgres|
+|[Multi-Node Setup](multi-setup/README.md)|Distribute a CDC Pipeline across
+multiple nodes|
+|[Iceberg on S3](iceberg/README.md)|Write records to Amazon S3 using Iceberg
+Table Format|
+|[Monitoring Example](monitoring/README.md)|Setup monitoring of complete CDC
+Pipeline|
+|[Use a Single Kafka Topic](single-topic/README.md)|Use a Single topic for
+multiple tables for CDC|
---
+++ single-topic/pg-connect.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -xe
+FILE=$1
+
+if test -f "$FILE"; then
+    export $(grep -v '^#' $FILE | xargs)
+fi
+
+payload() {
+    cat << EOF
+{
+    "name": "jdbc-sink-pg",
+    "config": {
+        "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",    
+        "tasks.max": "1",
+        "topics": "${TOPIC_PREFIX}_all_events",
+        "dialect.name": "PostgreSqlDatabaseDialect",
+        "connection.url": "jdbc:postgresql://$PGHOST:5432/postgres?user=postgres&password=postgres&sslMode=require",    
+        "transforms": "KeyFieldExample,unwrap",
+        "transforms.KeyFieldExample.type": "io.aiven.kafka.connect.transforms.ExtractTopic\$Key",
+        "transforms.KeyFieldExample.field.name": "__dbz__physicalTableIdentifier",
+        "transforms.KeyFieldExample.skip.missing.or.null": "true",
+        "transforms.unwrap.type": "io.debezium.connector.yugabytedb.transforms.YBExtractNewRecordState",   
+        "transforms.unwrap.drop.tombstones": "false",
+        "auto.create": "true",
+        "insert.mode": "upsert",
+        "pk.mode": "record_key",
+        "delete.enabled": "true",
+        "auto.evolve":"true"
+    }
+}
+EOF
+}
+
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d "$(payload)"
