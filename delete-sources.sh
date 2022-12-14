81f9733e8abec9c65a08bb4edd3907f5a152fed4
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-12-14 17:08:40 +0530

    moved files to correct locations

--- delete-sources.sh
+++
@@ -1,5 +0,0 @@
-curl -X DELETE localhost:8083/connectors/ybconnector_warehouse_district_item
-curl -X DELETE localhost:8083/connectors/ybconnector_stock
-curl -X DELETE localhost:8083/connectors/ybconnector_new_order
-curl -X DELETE localhost:8083/connectors/ybconnector_customer_oorder
-curl -X DELETE localhost:8083/connectors/ybconnector_order_line
